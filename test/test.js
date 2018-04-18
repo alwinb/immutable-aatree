feee988897fb5d27a82c49e4922f935588c36db1
Alwin Blok <alwinb@users.noreply.github.com>
2018-04-18 10:08:24 +0200

    some tests, changes

--- test/test.js
+++ test/test.js
@@ -1,51 +1,33 @@
-var AATree = require('../lib/aatree')
-const log = console.log.bind (console)
+const AATree = require('../lib/aatree')
+	, Node = AATree.__internal.Node
+	, EMPTY = AATree.__internal.EMPTY
+	, viz = require('./dot').dot
+  , log = console.log.bind (console)
 
-
-// Specify a comparison function
-// (This is idiomatic, the same thing Array.sort expects)
-
-var compare = function (a, b) {
-	return a < b ? -1 : a > b ? 1 : 0
+function toNode (o) {
+  if (o === EMPTY)
+    return { shape:'point' }
+  if (typeof o === 'object' && 'lv' in o)
+    return { shape:'record', label:o.k, rank:o.lv, children:['l', 'r'] }
 }
 
-// Lookup and insert/update values
-// -------------------------------
 
-var tree1 = new AATree(compare)
-var cursor = tree1.select('key1')
-console.log(cursor)
-// { found: false, key: 'key1', set: [Function: set] }
-
-var tree2 = cursor.set('value for key1')
-var cursor = tree2.select('key1')
-console.log(cursor)
-// { found: true, key: 'key1', value: 'value for key1', set: [Function: set] }
-
-var tree3 = tree2.select('key2').set('value for key2')
-var cursor = tree3.select('key2')
-
-
-// Call a function for each entry in the tree
-// ------------------------------------------
-
-log ('\ntest each')
-tree3.each (function (value, key) { console.log(key, ':', value) })
-
-
-
-// Lazy stream of pairs in ascending order
-// ---------------------------------------
-
-log ('\ntest stream')
-var stream = tree3.stream()
-
-do {
-	var pair = stream.next()
-	console.log(pair)
+let t = new AATree ()
+for (let i=0; i<200; i++) {
+  let k = Math.random ()
+  t = t.insert (k, 'value for '+k)
 }
-while (pair != null)
 
-// { key: 'key1', value: 'value for key1' }
-// { key: 'key2', value: 'value for key2' }
-// null
+for (let i=0; i<100; i++) {
+  let k = Math.random ()
+  let c = t.select (k) .next ()
+  if (c) t = c .unset ()
+}
+
+// let c = 0
+// t.forEach (_ => c++)
+// log (c)
+
+log(
+  viz (t.__reify (), toNode)
+)
